9f68ef0439d1d4523913f41df04ff84476fa0de7
Nischal <neeschal101@gmail.com>
2025-10-29 16:54:55 +0545

    return credentials in package

---
+++ build/lib/google_analytics_pkg/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class GoogleAnalyticsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'google_analytics_pkg'
---
+++ build/lib/google_analytics_pkg/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class GAConfiguration(models.Model):
+    property_id = models.CharField(max_length=50)
+    measurement_id = models.CharField(max_length=50)
+    api_secret = models.CharField(max_length=100)
+    credentials_json = models.TextField()  # Encrypted in real case
+
+class GAEventLog(models.Model):
+
+    event_name = models.CharField(max_length=100)
+    params = models.JSONField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+# class GAReportCache(models.Model):
+#     metric = models.CharField(max_length=100)
+#     dimension = models.CharField(max_length=100, null=True, blank=True)
+#     value = models.FloatField()
+#     retrieved_at = models.DateTimeField(auto_now=True)
