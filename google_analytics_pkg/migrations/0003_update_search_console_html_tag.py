76fb382a86b9eced2a98140916d1a89ed306f73b
Devloops Software Nepal <devloopssoftware@gmail.com>
2026-01-14 16:50:50 +0545

    Add search_console_html_tag field to gaconfiguration

--- google_analytics_pkg/migrations/0003_update_search_console_html_tag.py
+++ google_analytics_pkg/migrations/0003_update_search_console_html_tag.py
@@ -1,1 +1,16 @@
+from django.db import migrations, models
 
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('google_analytics_pkg', '0002_add_search_console_html_tag'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='gaconfiguration',
+            name='search_console_html_tag',
+            field=models.TextField(null=True, blank=True),
+        ),
+    ]
